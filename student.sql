a7e819f5ed286b05ed2d077194eabbe6db3a5def
B-Ramesh <ramschinna3@gmail.com>
2026-08-19 15:47:51 +0530

    this is my fourth commit

--- student.sql
+++ student.sql
@@ -1,6 +1,8 @@
-create table Student(
+create table Student_1(
     roll_number number(10),
     stu_name varchar2(10),
     stu_fname varchar2(10),
-    stu_mobilenumber varchar2(10)
-)
+    stu_mobilenumber varchar2(10),
+    stu_age number(2)
+);
+select * from emp;
